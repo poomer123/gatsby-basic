7aa6604be98c909e03acf1f692474216661dfb22
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-04-25 15:28:20 +0700

    testing props

---
+++ src/components/box.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Box = (props) => {
+    const { num } = props
+    return (
+        <div>
+            box show num : {num}
+        </div>
+    )
+}
+
+export default Box
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import Header from '../components/header'
+import Box from '../components/Box'
 
 const About = () => {
     const [num, setNum] = useState(0)
@@ -8,7 +9,7 @@ const About = () => {
         <div>
             <Header />
             <h1>About page</h1>
-            <p>Num : {num}</p>
+            <Box num={num} />
             <button onClick={() => setNum(num + 1)}>+</button>
 		    <button onClick={() => setNum(num > 0 ? num - 1 : num)}>-</button>
         </div>
