8257ffcbbf4ac5100e02f09a3691759d51e3ed1c
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-04-25 15:37:44 +0700

    style module

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import Header from '../components/header'
 import Box from '../components/Box'
+import style from '../../static/style.module.css'
 
 const About = () => {
     const [num, setNum] = useState(0)
@@ -10,7 +11,7 @@ const About = () => {
             <Header />
             <h1>About page</h1>
             <Box num={num} />
-            <button onClick={() => setNum(num + 1)}>+</button>
+            <button className={style.redBtn} onClick={() => setNum(num + 1)}>+</button>
 		    <button onClick={() => setNum(num > 0 ? num - 1 : num)}>-</button>
         </div>
     )
