243980b13f5556b5ac7c588e98f51576bc0918f0
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-04-25 15:24:30 +0700

    functional component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import Header from '../components/header'
 
-export default () => (
-    <div>
-        <Header />
-        <h1>About page</h1>
-    </div>
-)
+const About = () => {
+    const [num, setNum] = useState(0)
+    
+    return (
+        <div>
+            <Header />
+            <h1>About page</h1>
+            <p>Num : {num}</p>
+            <button onClick={() => setNum(num + 1)}>+</button>
+		    <button onClick={() => setNum(num > 0 ? num - 1 : num)}>-</button>
+        </div>
+    )
+}
+
+export default About
