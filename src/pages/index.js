170687fe7d2388025f202e7c5ea054cb56f695ef
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-04-25 15:09:48 +0700

    testing set state

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,24 @@ class Home extends Component {
             <div>
                 <Header />
                 <h1>Home page</h1>
-                {this.state.num}
+                <p>Num : {this.state.num}</p>
+                <button onClick={() => this.addNum()}>+</button>
+		        <button onClick={() => this.decreaseNum()}>-</button>
             </div>
         )
     }
+
+    addNum () {
+		this.setState({
+            num: this.state.num + 1
+        })
+	}
+
+	decreaseNum () {
+		this.setState({
+            num: this.state.num > 0 ? this.state.num-1 : this.state.num
+        })
+	}
 }
 
 export default Home
